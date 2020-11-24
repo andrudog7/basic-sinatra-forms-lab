f07a61f94419d8b453cdae24d19c66b7939f9555
--add <andrewksmoker@gmail.com>
2020-11-24 14:51:19 -0500

    commit tree f44db2982de0f6d762f125b0a2f67bd2e6443b61

--- app.rb
+++ app.rb
@@ -2,5 +2,18 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+    get '/newteam' do 
+        erb :newteam 
+    end
 
+    post '/newteam' do
+        @name = params[:team_name] 
+        @coach = params[:coach]
+        @pointguard = params[:point_guard]
+        @shootingguard = params[:shooting_guard]
+        @smallforward = params[:small_forward]
+        @powerforward = params[:power_forward]
+        @center = params[:center]     
+        erb :team
+    end
 end
